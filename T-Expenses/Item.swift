e2af4aa56e1c07058d85a1063d683ef5b764267c
Roberto Esposito <roberto.esposito@unito.it>
2025-01-01 12:16:25 +0100

    Initial Commit

---
+++ T-Expenses/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  T-Expenses
+//
+//  Created by Roberto Esposito on 01/01/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ T-ExpensesTests/T_ExpensesTests.swift
@@ -0,0 +1,17 @@
+//
+//  T_ExpensesTests.swift
+//  T-ExpensesTests
+//
+//  Created by Roberto Esposito on 01/01/25.
+//
+
+import Testing
+@testable import T_Expenses
+
+struct T_ExpensesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
